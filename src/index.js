26a44b37c5265dc4983022734024f980b862b36d
JN <jyun16@gmail.com>
2026-04-27 22:05:50 +0900

    instanceName

--- src/index.js
+++ src/index.js
@@ -51,6 +51,8 @@ export function isMap(v) { return _.isMap(v) }
 
 export function includes(t, v, i) { return _.includes(t, v, i) }
 
+export function instanceName(o) { obj?.constructor?.name || typeof obj }
+
 export function expandRange(r) {
 	return r.split(',').flatMap(s => {
 		const t = s.trim()
